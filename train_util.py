caf0efe60e67ef6d1163da9b2def4aa124a428c7
Pedro Henrique <pedro00dk@gmail.com>
2017-11-02 08:52:08 -0300

    Remove method to test variations in a single configuration

--- train_util.py
+++ train_util.py
@@ -20,9 +20,11 @@ def validate_model(model, sample_folds, original_folds, runs=5, test_original_fo
     :return: a dictionary with expected_results as keys and the result as values in arrays of runs * k_folds size
     """
     results = {name: [] for name in result_data}
+
     for run, i in itertools.product(range(runs), range(len(sample_folds))):
         if verbose:
             print('run %d of %d, fold %d of %d' % (run + 1, runs, i + 1, len(sample_folds)))
+
         train_folds = sample_folds[i + 1:len(sample_folds)] + sample_folds[0:i]
         train_instances = [instance for fold in train_folds for instance in fold[0]]
         train_classes = [clazz for fold in train_folds for clazz in fold[1]]
@@ -50,20 +52,21 @@ def validate_model(model, sample_folds, original_folds, runs=5, test_original_fo
             results['roc'].append([fpr, tpr, area])
 
     results = {name: np.array(r) if name != 'roc' else [np.array(d) for d in r] for name, r in results.items()}
+
     return results
 
 
-def validate_model_configuration_variations(model_class, sample_folds, original_folds, base_configuration,
-                                            variation_name, variation_values, runs=5, test_original_fold=True,
-                                            verbose=True):
+def validate_model_configurations_variations(model_class, sample_folds, original_folds, configurations,
+                                             variation_name, variation_values, runs=5, test_original_fold=True,
+                                             verbose=True):
     """
-    Tests multiples variations of a configuration of a model using k-fold cross validation over already split folds
-    returning information about the variations.
+    Tests multiples variations of multiples configurations of a model using k-fold cross validation over already split
+    folds returning information about the variations.
 
     :param model_class: scikit-learn model class to validate
     :param sample_folds: list of tuples with two lists each, one of instances and other of instance's classes
     :param original_folds: same as sample folds but with no processing, can be None if test_original_folds is False
-    :param base_configuration: base of the configuration to test
+    :param configurations: base of the configurations to test the variations
     :param variation_name: variation attribute name in model
     :param variation_values: variation values to be tested
     :param runs: number of runs over all folds
@@ -73,43 +76,22 @@ def validate_model_configuration_variations(model_class, sample_folds, original_
              of test results.
     """
     configuration_variations_results = []
+
     result_data = {'mse', 'matrix', 'roc'}
-    for i, variation_value in enumerate(variation_values):
-        if verbose:
-            print('variation %d of %d | %s -> %s' % (i + 1, len(variation_values), variation_name, variation_value))
-        configuration = base_configuration.copy()
-        configuration[variation_name] = variation_value
-        model = model_class(**configuration)
-        result = validate_model(model, sample_folds, original_folds, runs, test_original_fold, result_data, verbose)
-        configuration_variations_results.append((configuration, result))
-    return configuration_variations_results
 
-
-def validate_model_configurations_variations(model_class, sample_folds, original_folds, configurations, variation_name,
-                                             variation_values, runs=5, test_original_fold=True, verbose=True):
-    """
-    Tests multiples variations of multiples configurations of a model using k-fold cross validation over already split
-    folds returning information about the variations.
-
-    :param model_class: scikit-learn model class to validate
-    :param sample_folds: list of tuples with two lists each, one of instances and other of instance's classes
-    :param original_folds: same as sample folds but with no processing, can be None if test_original_folds is False
-    :param configurations: configurations to test the variations
-    :param variation_name: variation attribute name in model
-    :param variation_values: variation values to be tested
-    :param runs: number of runs over all folds
-    :param test_original_fold: if should use sample_folds or original_original_folds test set
-    :param verbose: if should print the current iteration
-    :return: list of tuples with the configuration and dictionary with mse, matrix and roc keys and corresponding arrays
-             of test results.
-    """
-    configurations_variations_results = []
     for i, configuration in enumerate(configurations):
         if verbose:
             print('configuration %d of %d' % (i + 1, len(configurations)))
             print('-> %s' % configuration)
-        result = validate_model_configuration_variations(model_class, sample_folds, original_folds, configuration,
-                                                         variation_name,
-                                                         variation_values, runs, test_original_fold, verbose)
-        configurations_variations_results.extend(result)
-    return configurations_variations_results
+
+        for j, variation_value in enumerate(variation_values):
+            if verbose:
+                print('variation %d of %d | %s -> %s' % (j + 1, len(variation_values), variation_name, variation_value))
+
+            configuration = configuration.copy()
+            configuration[variation_name] = variation_value
+            model = model_class(**configuration)
+            result = validate_model(model, sample_folds, original_folds, runs, test_original_fold, result_data, verbose)
+            configuration_variations_results.append((configuration, result))
+
+    return configuration_variations_results
